0005923b0a58ec77e2b4c2ce44f0f52a70735c0f
Zia Syed <zia.syed@ericsson.com>
2015-03-28 15:52:40 -0700

    added createUser script and fixed service defition

--- base/getconf.sh
+++ base/getconf.sh
@@ -1,1 +1,2 @@
+#!/bin/bash -e
 /usr/local/bin/confd -onetime -config-file /app/confd.toml -node $ETCD
---
+++ gateway/createUser.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+radosgw-admin user create --uid=johndoe --display-name="John Doe" --email=john@example.com
