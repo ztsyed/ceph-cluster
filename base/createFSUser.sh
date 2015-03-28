fedf5bd4a115bb50b961e99605a43145ff6d4e33
Zia Syed <zia.syed@ericsson.com>
2015-03-28 16:12:05 -0700

    added script to createFSUser

---
+++ base/createFSUser.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+ceph auth get-or-create client.erix osd 'allow rw' mon 'allow r' > keyring.erix
+echo "Got-or-Created Key for Erix"
+cat keying.erix
+
+echo "NFS Mount\nsudo mount -t ceph ceph-monitor.skydns.local:6789:/ /mnt/mycephfs -o name=erix,secret=xxx"
+echo "OR\n"
+echo "NFS Mount\nsudo mount -t ceph ceph-monitor.skydns.local:6789:/ /mnt/mycephfs -o name=erix,secretfile=keyring.erix"
